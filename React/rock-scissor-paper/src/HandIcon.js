3b4d1d188f7ae7b41b03e1e425eb76122265e499
Lilly1757 <seungji34@gmail.com>
2024-10-02 14:09:14 +0900

    Feat: prop를 활용한 가위바위보 이미지 삽입 및 변경

--- React/rock-scissor-paper/src/HandIcon.js
+++ React/rock-scissor-paper/src/HandIcon.js
@@ -2,8 +2,14 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 
-function HandIcon() {
-  return <img src={rockImg} alt="rock" />;
+const HAND_IMAGES={
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+function HandIcon({value}) {
+  const src = HAND_IMAGES[value];
+  return <img src={src} alt={value} />;
 }
 
 export default HandIcon;
