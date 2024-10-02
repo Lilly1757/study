d00443e7d7f66b1aeceff3f4fc739de2e6485c85
Lilly1757 <seungji34@gmail.com>
2024-10-02 17:14:53 +0900

    Feat: onClick을 활용한 button 이벤트 핸들러 등록

--- React/dicegame/src/App.js
+++ React/dicegame/src/App.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Dice from "./Dice";
 import Button from "./Button";
 
+function Random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = Random(6);
+    setNum(nextNum);
+  };
+  const handleClearClick = () => {
+    setNum(1);
+  };
+
   return (
     <div>
-      <Button>던지기</Button>
-      <Button>처음부터</Button>
-      <Dice color="red" num={2} />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <Dice color="red" num={num} />
     </div>
   );
 }
