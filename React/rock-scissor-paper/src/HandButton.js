e915b87ea6c5c0cae43fa63e529b9dc94af5f365
Lilly1757 <seungji34@gmail.com>
2024-10-03 23:13:38 +0900

    Style: Button 스타일 적용

--- React/rock-scissor-paper/src/HandButton.js
+++ React/rock-scissor-paper/src/HandButton.js
@@ -1,9 +1,24 @@
 import HandIcon from "./HandIcon";
+import Purple from "./assets/purple.svg";
+
+const ButtonStyle = {
+  width: "166px",
+  height: "166px",
+  border: "none",
+  outline: "none",
+  textAlign: "center",
+  cursor: "pointer",
+  backgroundColor: "transparent",
+  backgroundImage: `url('${Purple}')`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundSize: "contain",
+};
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
-    <button onClick={handleClick}>
+    <button style={ButtonStyle} onClick={handleClick}>
       <HandIcon value={value} />
     </button>
   );
