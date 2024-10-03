72559ebd662551072fd3c96f2e66ac182deea39b
Lilly1757 <seungji34@gmail.com>
2024-10-03 20:52:18 +0900

    Refactor: 코드 정리

--- React/dicegame/src/App.js
+++ React/dicegame/src/App.js
@@ -7,30 +7,18 @@ function Random(n) {
 }
 
 function App() {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
-  const [otherNum, setOtherNum] = useState(1);
-  const [otherSum, setOtherSum] = useState(0);
-  const [otherGameHistory, setOtherGameHistory] = useState([]);
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
 
   const handleRollClick = () => {
-    const nextNum = Random(6);
-    const nextOtherNum = Random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    setGameHistory([...gameHistory, nextNum]);
-    setOtherNum(nextOtherNum);
-    setOtherSum(otherSum + nextOtherNum);
-    setOtherGameHistory([...otherGameHistory, nextOtherNum]);
+    const myNum = Random(6);
+    const otherNum = Random(6);
+    setMyHistory([...myHistory, myNum]);
+    setOtherHistory([...otherHistory, otherNum]);
   };
   const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-    setOtherNum(1);
-    setOtherSum(0);
-    setOtherGameHistory([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   };
 
   return (
@@ -43,16 +31,12 @@ function App() {
         <Board
           name="나"
           color="blue"
-          num={num}
-          sum={sum}
-          gameHistory={gameHistory}
+          gameHistory={myHistory}
         />
         <Board
           name="상대"
           color="red"
-          num={otherNum}
-          sum={otherSum}
-          gameHistory={otherGameHistory}
+          gameHistory={otherHistory}
         />
       </div>
     </div>
--- React/dicegame/src/Board.js
+++ React/dicegame/src/Board.js
@@ -1,6 +1,8 @@
 import Dice from "./Dice";
 
-function Board({ name, color, num, sum, gameHistory }) {
+function Board({ name, color, gameHistory }) {
+  const sum = gameHistory.reduce((a, b) => a + b, 0);
+  const num = gameHistory[gameHistory.length - 1];
   return (
     <div>
       <h2>{name}</h2>
