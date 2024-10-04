f0b5af432267bbe7febc16ca0998394ce95789da
Lilly1757 <seungji34@gmail.com>
2024-10-04 22:46:49 +0900

    Feat: Delete 버튼 추가

--- React/foodit/src/components/App.js
+++ React/foodit/src/components/App.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import FoodList from "./FoodList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState(mockItems)
   const handleNewestClick = () => setOrder("createdAt");
   const handleCalorieClick = () => setOrder("calorie");
+  const handleDelete = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  }
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete}/>
     </div>
   );
 }
