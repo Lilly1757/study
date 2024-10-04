441553cde4f7d58e2900e9fd9ee7824b13771e34
Lilly1757 <seungji34@gmail.com>
2024-10-04 16:38:56 +0900

    Feat: Delete 기능 추가

--- React/moviepedia/src/components/App.js
+++ React/moviepedia/src/components/App.js
@@ -1,14 +1,18 @@
-import ReviewList from './ReviewList';
-import items from '../mock.json';
-import {useState} from 'react';
+import ReviewList from "./ReviewList";
+import mockItems from "../mock.json";
+import { useState } from "react";
 
 function App() {
-  const [order, setOrder] = useState('createdAt');
-  const sortedItems = items.sort((a, b) => (b[order] - a[order]));
+  const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState(mockItems);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-  const handleNewestClick = () => setOrder('createdAt')
-  const handleBestClick = () => setOrder('rating')
-
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
 
   return (
     <div>
@@ -16,7 +20,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
