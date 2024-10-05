97275113c0fb4e98449bd23cbbdb71426168cd49
Lilly1757 <seungji34@gmail.com>
2024-10-05 11:32:26 +0900

    Frat: Fetch를 활용한 데이터 추가

--- React/moviepedia/src/components/App.js
+++ React/moviepedia/src/components/App.js
@@ -1,10 +1,10 @@
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
 import { useState } from "react";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -13,12 +13,17 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
+  const handleLoadClick = async () => {
+    const {reviews} = await getReviews();
+    setItems(reviews);
+  }
 
   return (
     <div>
       <div>
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
+        <button onClick={handleLoadClick}>불러오기</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
