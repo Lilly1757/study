9be2f44d063a4ae3be4797a1bede4b5e958b5635
Lilly1757 <seungji34@gmail.com>
2024-10-07 22:27:45 +0900

    Feat: 초기화 버튼 구현

--- React/foodit/src/components/FileInput.js
+++ React/foodit/src/components/FileInput.js
@@ -1,10 +1,31 @@
-function FileInput({ name, value, onChange }) {
-  const handleChange = (e) => {
-    const nextValue = e.target.files[0]
-    onChange(name, nextValue);
-  }
+import { useRef } from "react";
 
-  return <input type="file" onChange={handleChange} />;
+function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
+  };
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && (
+        <button type="button" onClick={handleClearClick}>
+          X
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default FileInput;
