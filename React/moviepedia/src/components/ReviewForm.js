59389cd3a272d9b48e39dd017b2597ab9885ffda
Lilly1757 <seungji34@gmail.com>
2024-10-07 15:58:27 +0900

    Refactor: Form 코드 수정

--- React/moviepedia/src/components/ReviewForm.js
+++ React/moviepedia/src/components/ReviewForm.js
@@ -2,37 +2,35 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [value, setValue] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValue((preValues) => ({
+      ...preValues,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
-      title,
-      rating,
-      content,
-    });
+    console.log(value);
   };
 
   return (
     <form className="rewiewForm" onSubmit={handleSubmit}>
-      <input value={title} onChange={handleTitleChange}></input>
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input name="title" value={value.title} onChange={handleChange}></input>
+      <input
+        name="rating"
+        type="number"
+        value={value.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={value.content} onChange={handleChange} />
       <button type="submit">확인</button>
     </form>
   );
