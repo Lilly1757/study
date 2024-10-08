ec3a78f4d37fb491866e97992f6bb812e8b5f04f
Lilly1757 <seungji34@gmail.com>
2024-10-08 20:04:42 +0900

    Feat: 별점 컴포넌트 생성

---
+++ React/moviepedia/src/components/Rating.js
@@ -0,0 +1,19 @@
+import "./Rating.css";
+const RATINGS = [1, 2, 3, 4, 5];
+
+function Star({ selected = false }) {
+  const className = `Rating-start ${selected ? "selected" : ""}`;
+  return <span className={className}>*</span>;
+}
+
+function Rating({ value = 0 }) {
+  return (
+    <div>
+      {RATINGS.map((rating) => (
+        <Star key={rating} selected={value >= rating} />
+      ))}
+    </div>
+  );
+}
+
+export default Rating;
